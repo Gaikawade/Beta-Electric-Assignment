6fbe28905a95daa6c9f142b2685782dabb57e061
Gaikawade <gmhs6199@gmail.com>
2022-12-13 14:15:14 +0530

    input fields in fav

--- client/src/Components/Fav.jsx
+++ client/src/Components/Fav.jsx
@@ -1,15 +1,60 @@
-import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
-export default function Home() {
+export default function Fav() {
+    const [packageName, setPackageName] = useState("");
+    const [result, setResult] = useState("");
+    console.log(packageName);
+
+    const fetchPackages = (packageName = "") => {
+        axios
+            .get(`https://api.npms.io/v2/search?q=${packageName}`)
+            .then((res) => {
+                setResult(res.data.results);
+                console.log(res.data);
+            })
+            .catch((err) => console.log(err));
+    };
+    // useEffect(() => {
+    //     return () => {
+    //         axios
+    //             .get(
+    //                 `https://api.npms.io/v2/search?${
+    //                     packageName && `&q=${packageName}`
+    //                 }`
+    //             )
+    //             .then((res) => console.log(res))
+    //             .catch((err) => console.log(err));
+    //     };
+    // }, []);
+    const { arr } = [];
+    const showPackages = (result) => {
+        result.forEach((x) => {
+            arr.push(
+                <div key={arr.indexOf(x)}>
+                    <p> name: {x.name}</p>
+                </div>
+            );
+        });
+        console.log(arr);
+        return { arr };
+    };
     return (
         <div className="flex h-screen flex-col items-center justify-center">
             <p className="font-bold">Search for NPM Packages</p>
             <input
                 className="border-2 border-stale-700 w-96 "
-                type="text"
-                name="name"
+                onChange={(e) => {
+                    setPackageName(e.target.value);
+                }}
+                onClick={fetchPackages}
+                value={packageName}
             />
+            <ul>
+                <li>{showPackages}</li>
+            </ul>
             <p className="font-bold">Result</p>
+            <div></div>
             <p className="font-bold">Why is this your fav?</p>
             <input
                 className="border-2 border-stale-700 h-36 w-96"
--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router";
+
 export default function Home() {
   const navigate = useNavigate();
 
