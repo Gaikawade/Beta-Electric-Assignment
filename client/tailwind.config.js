7d9d4fa1d23a406eca722452c70156b6ff08551b
Gaikawade <gmhs6199@gmail.com>
2022-12-15 01:05:11 +0530

    edit fev page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
             colors: {
                 "button_color": "#6558F5",
                 "button_text": "#FFFFFF",
-                "text_color": "#4B5C6B"
+                "text_color": "#4B5C6B",
+                "box_color": "#C5CED6"
             },
         },
     },
