0682b7af38cdbda85b32d46277adb1e26c200826
Gaikawade <gmhs6199@gmail.com>
2022-12-15 01:39:45 +0530

    extract package names from API call

--- client/src/Components/Fav.jsx
+++ client/src/Components/Fav.jsx
@@ -4,6 +4,14 @@ import { useState } from "react";
 export default function Fav() {
     const [packageName, setPackageName] = useState("");
     const [result, setResult] = useState("");
+    const name = [];
+
+    const handleChange = (evt) => {
+        setPackageName(evt.target.value);
+        fetchPackages(packageName);
+        showPackages();
+        console.log(name);
+    };
 
     const fetchPackages = (packageName = "") => {
         axios
@@ -14,36 +22,41 @@ export default function Fav() {
             )
             .then((res) => {
                 setResult(res.data.results);
-                console.log(result);
+                // console.log(result);
             })
             .catch((err) => console.log(err));
     };
+
+    const showPackages = () => {
+        for (let i = 0; i < result.length; i++) {
+            name.push(result[i].package.name);
+        }
+    };
+
+    // const displayPackages = () => {
+    //     return name.map((ele) => <li>{ele}</li>);
+    // };
 
     return (
         <div className="mx-16 my-12 text-text_color">
             <p className="font-bold">Search for NPM Packages</p>
             <input
-                className="border-2 border-stale-700 w-full"
-                onChange={(e) => {
-                    setPackageName(e.target.value);
-                }}
-                onClick={fetchPackages}
+                className="border-2 border-box_color w-full"
+                onChange={handleChange}
                 value={packageName}
             />
             <p className="font-bold my-9">Result</p>
-            <div className="overflow-auto h-28">
+            <table className="overflow-auto h-28">
                 {/* {
-                    result.map((curEle) => 
-                        <div>
-                            <li>curEle.package.name</li>
-                        </div>
-                    )
+                    name.map((ele)=>(
+                        <tr>
+                            <td>{ele}</td>
+                        </tr>
+                    ))
                 } */}
-            </div>
+            </table>
             <p className="font-bold">Why is this your fav?</p>
-            <input
-                className="border-2 border-stale-700 h-36 w-full"
-            />
+            <input className="border-2 border-box_color h-36 w-full" />
             <br />
             <button class="bg-button_color text-white font-bold py-2 px-4 -mx-0 my-4 rounded -right-0">
                 Submit
