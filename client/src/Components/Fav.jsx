6756db6fc0fe3936d2b9d722b968453d7d04189c
Gaikawade <gmhs6199@gmail.com>
2022-12-15 17:57:41 +0530

    showing reasult

--- client/src/Components/Fav.jsx
+++ client/src/Components/Fav.jsx
@@ -1,15 +1,17 @@
 import axios from "axios";
 import { useState } from "react";
+import {BiCircle} from "react-icons/bi"
 
 export default function Fav() {
     const [packageName, setPackageName] = useState("");
     const [result, setResult] = useState("");
+    const [list, setList] = useState([]);
     const name = [];
 
     const handleChange = (evt) => {
         setPackageName(evt.target.value);
         fetchPackages(packageName);
-        showPackages();
+        extractPackageNames();
         console.log(name);
     };
 
@@ -27,35 +29,30 @@ export default function Fav() {
             .catch((err) => console.log(err));
     };
 
-    const showPackages = () => {
+    const extractPackageNames = () => {
         for (let i = 0; i < result.length; i++) {
             name.push(result[i].package.name);
         }
+        setList(name);
     };
-
-    // const displayPackages = () => {
-    //     return name.map((ele) => <li>{ele}</li>);
-    // };
 
     return (
         <div className="mx-16 my-12 text-text_color">
             <p className="font-bold">Search for NPM Packages</p>
             <input
-                className="border-2 border-box_color w-full"
+                className="border-2 border-box_color w-full mb-7"
                 onChange={handleChange}
                 value={packageName}
             />
-            <p className="font-bold my-9">Result</p>
-            <table className="overflow-auto h-28">
-                {/* {
-                    name.map((ele)=>(
-                        <tr>
-                            <td>{ele}</td>
-                        </tr>
-                    ))
-                } */}
-            </table>
-            <p className="font-bold">Why is this your fav?</p>
+            <p className="font-bold">Result</p>
+            <div className="flex flex-col overflow-auto h-28 list-none mt-3">
+                {list.map((ele) => (
+                    <li className="flex ">
+                        <BiCircle className="flex flex-col"/>{ele}
+                    </li>
+                ))}
+            </div>
+            <p className="font-bold mt-8">Why is this your fav?</p>
             <input className="border-2 border-box_color h-36 w-full" />
             <br />
             <button class="bg-button_color text-white font-bold py-2 px-4 -mx-0 my-4 rounded -right-0">
