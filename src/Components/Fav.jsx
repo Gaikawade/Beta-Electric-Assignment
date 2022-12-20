eb07b67ea7b657bdad3a81eb5ba010441066a345
Gaikawade <gmhs6199@gmail.com>
2022-12-20 13:39:59 +0530

    storing in local storage

--- src/Components/Fav.jsx
+++ src/Components/Fav.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Fav() {
     const [packageName, setPackageName] = useState("");
     const [result, setResult] = useState("");
     const [list, setList] = useState([]);
-    // const [selectedPackage, setSelectedPackage] = useState("");
+    const [selectedPackage, setSelectedPackage] = useState("");
+    const [whyFav, setWhyFav] = useState("");
     const name = [];
+
+    const navigate = useNavigate();
 
     const handleChange = (evt) => {
         setPackageName(evt.target.value);
@@ -36,11 +40,14 @@ export default function Fav() {
         setList(name);
     };
 
-    const handleCheckboxClick = (e) => {
-        console.log(e)
-        const { value, checked } = e.target;
-        console.log(value, checked);
-    };
+    function AddFav() {
+        navigate("/");
+    }
+
+    function handleSubmit() {
+        localStorage.setItem(selectedPackage, whyFav);
+        AddFav();
+    }
 
     return (
         <div className="mx-16 my-12 text-text_color">
@@ -54,7 +61,12 @@ export default function Fav() {
             <div className=" overflow-auto h-28 mt-3 list-none">
                 {list.map((ele) => (
                     <li>
-                        <input type="checkbox" className="rounded-full" onChange={handleCheckboxClick} />
+                        <input
+                            type="checkbox"
+                            className="rounded-full"
+                            onChange={() => setSelectedPackage(ele)}
+                        />
+                        {console.log(selectedPackage)}
                         <lable>
                             <span>{ele}</span>
                         </lable>
@@ -62,9 +74,15 @@ export default function Fav() {
                 ))}
             </div>
             <p className="font-bold mt-8">Why is this your fav?</p>
-            <input className="border-2 border-box_color h-36 w-full " />
+            <input
+                className="border-2 border-box_color h-36 w-full "
+                onChange={(evt) => setWhyFav(evt.target.value)}
+            />
             <br />
-            <button class="bg-button_color text-white font-bold py-2 px-4 -mx-0 my-4 rounded -right-0">
+            <button
+                class="bg-button_color text-white font-bold py-2 px-4 -mx-0 my-4 rounded -right-0"
+                onClick={handleSubmit}
+            >
                 Submit
             </button>
         </div>
